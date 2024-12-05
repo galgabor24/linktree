f265723227af354641e863a0c0417f6dbc96ff2f
galgabor24 <79318879+galgabor24@users.noreply.github.com>
2024-12-05 21:57:57 +0000

    Update Background.tsx

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -5,7 +5,6 @@ export function Background() {
   return (
     <div className="fixed inset-0 -z-10">
       <BackgroundImage />
-      <div className="absolute inset-0 bg-[radial-gradient(circle_at_50%_120%,rgba(120,119,198,0.1),rgba(255,255,255,0))]" />
     </div>
   );
 }
